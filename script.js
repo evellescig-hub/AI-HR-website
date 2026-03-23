3d5847c1707fab09036f8b9e4eaac6aa7172b2c7
evellescig-hub <evellescig@gmail.com>
2026-03-23 15:11:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-const ctx = document.getElementById('aiChart');
+const ctx1 = document.getElementById('chart1');
 
-new Chart(ctx, {
+new Chart(ctx1, {
     type: 'bar',
     data: {
         labels: ['Hiring Speed', 'Cost Reduction', 'Accuracy', 'Employee Insights'],
         datasets: [{
-            label: 'Impact of AI in HR (%)',
+            label: 'Impact of AI (%)',
             data: [80, 70, 85, 75]
         }]
-    },
-    options: {
-        responsive: true
+    }
+});
+
+const ctx2 = document.getElementById('chart2');
+
+new Chart(ctx2, {
+    type: 'pie',
+    data: {
+        labels: ['Recruitment', 'Payroll', 'Analytics', 'Employee Engagement'],
+        datasets: [{
+            data: [35, 20, 25, 20]
+        }]
     }
 });
